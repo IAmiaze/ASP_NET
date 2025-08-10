168f4b57e2100f8aafd0a359ea18b6ba676b46d0
IAmiaze <ayub@erainfotechbd.com>
2025-08-10 18:04:59 +0600

    Commit

--- BlazorApp1/obj/Debug/net9.0/BlazorApp1.AssemblyInfo.cs
+++ BlazorApp1/obj/Debug/net9.0/BlazorApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ea7a624e495cd09540fdf7d13eeb1c264d182e99")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b93e1a3e151ce79824d26161fde2d2b7609af650")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
